af5206543f85e76b760fdfd0e274faf9244a2f81
Steve Sim <stevesim@hotmail.com>
2020-01-27 15:40:56 +0000

    Bugfixing with route and gh-pahes #1

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -5,35 +5,34 @@ import Home from "../home";
 import MissionPanel from "../missions/mission-panel";
 import LaunchPanel from "../launches/launch-panel";
 import FullDetailedLaunch from '../launches/full-detail-launch';
+import env from '../../../package.json';
 const logo = require("../../spaceX_logo.png");
 
 class NavBarComponent extends Component {
   render() {
     return (
-      <Router>
+      <Router basename="/">
         <Navbar bg='dark' variant='dark'>
-          <Navbar.Brand href='/'>
+          <Navbar.Brand href={`${env.homepage}`}>
             <img alt='spaceX' src={logo} width='150' height='45' className='d-inline-block align-top' />
           </Navbar.Brand>
 
           <Nav className='mr-auto'>
-            <Nav.Link href='/missions'>Missions</Nav.Link>
-            <Nav.Link href='/launches'>Launches</Nav.Link>
+            <Nav.Link href={`${env.homepage}/missions`}>Missions</Nav.Link>
+            <Nav.Link href={`${env.homepage}/launches`}>Launches</Nav.Link>
           </Nav>
         </Navbar>
         <Switch>
-          <Route exact path='/'>
+          <Route path=''>
             <Home />
           </Route>
-          <Route exact path='/missions'>
+          <Route path='missions'>
             <MissionPanel />
           </Route>
-          <Route exact path='/launches'>
+          <Route path='launches'>
             <LaunchPanel />
           </Route>
-          <Route exact path='/launch/:flight_number' component={FullDetailedLaunch}>
-            
-          </Route>
+          <Route exact path='launch/:flight_number' component={FullDetailedLaunch}></Route>
         </Switch>
       </Router>
     );
