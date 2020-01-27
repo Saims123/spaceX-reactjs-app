f991d4930527b18009099ebfbd77981750ae615d
Saims123 <stevesim@hotmail.com>
2020-01-27 16:39:15 +0000

    Updates

---
+++ precache-manifest.ee9b06cbbad2494fa751dc4b74fa889b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "211a3d61f015a2471b7d982ba1ee31aa",
+    "url": "spaceX-reactjs-app/index.html"
+  },
+  {
+    "revision": "68b27341893e75bd5f3f",
+    "url": "spaceX-reactjs-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "048422b8ddd7e8fb294e",
+    "url": "spaceX-reactjs-app/static/css/main.cad3dd14.chunk.css"
+  },
+  {
+    "revision": "68b27341893e75bd5f3f",
+    "url": "spaceX-reactjs-app/static/js/2.0a45b9c2.chunk.js"
+  },
+  {
+    "revision": "2a9ca498f958dc914ee8331f2f1e5a45",
+    "url": "spaceX-reactjs-app/static/js/2.0a45b9c2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "048422b8ddd7e8fb294e",
+    "url": "spaceX-reactjs-app/static/js/main.83a23864.chunk.js"
+  },
+  {
+    "revision": "c3dc4bcf90fe83f67ae8",
+    "url": "spaceX-reactjs-app/static/js/runtime-main.cfd61649.js"
+  },
+  {
+    "revision": "32e55f1080d9a50aeb430e767fd5d65e",
+    "url": "spaceX-reactjs-app/static/media/no_image_available.32e55f10.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spaceX-reactjs-app/precache-manifest.b3742350bed03c62b2cc3bcc9cc3ab24.js"
+  "spaceX-reactjs-app/precache-manifest.ee9b06cbbad2494fa751dc4b74fa889b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spaceX-reactjs-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("spaceX-reactjs-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
