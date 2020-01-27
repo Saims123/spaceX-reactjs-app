4746a6afa6dcda5fc451f68624f2f0f6d152c2e0
Saims123 <stevesim@hotmail.com>
2020-01-27 04:10:30 +0000

    Changed idea from modal to normal component

---
+++ src/components/launches/full-detail-launch.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class FullDetailedLaunch extends Component {
+  
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default FullDetailedLaunch;
