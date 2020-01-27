c089a87033faa45f4b69ea41ec360c5f6d699322
Saims123 <stevesim@hotmail.com>
2020-01-27 16:52:56 +0000

    Updates

---
+++ precache-manifest.469fd8e812174b174b7af06e9d4b4e26.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ebffa6a10d10e49c66df20255608e7c",
+    "url": "/spaceX-reactjs-app/index.html"
+  },
+  {
+    "revision": "68b27341893e75bd5f3f",
+    "url": "/spaceX-reactjs-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "0b7c9b5984d0be0dc085",
+    "url": "/spaceX-reactjs-app/static/css/main.cad3dd14.chunk.css"
+  },
+  {
+    "revision": "68b27341893e75bd5f3f",
+    "url": "/spaceX-reactjs-app/static/js/2.0a45b9c2.chunk.js"
+  },
+  {
+    "revision": "2a9ca498f958dc914ee8331f2f1e5a45",
+    "url": "/spaceX-reactjs-app/static/js/2.0a45b9c2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0b7c9b5984d0be0dc085",
+    "url": "/spaceX-reactjs-app/static/js/main.8cd8dc30.chunk.js"
+  },
+  {
+    "revision": "a1c6c1551b7a4f267bc2",
+    "url": "/spaceX-reactjs-app/static/js/runtime-main.8782af39.js"
+  },
+  {
+    "revision": "32e55f1080d9a50aeb430e767fd5d65e",
+    "url": "/spaceX-reactjs-app/static/media/no_image_available.32e55f10.png"
+  }
+]);
