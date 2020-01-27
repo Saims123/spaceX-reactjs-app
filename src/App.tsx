3ed13c0e727857f306d2fca6c7be8fcdb7c59ba4
Saims123 <stevesim@hotmail.com>
2020-01-27 02:25:49 +0000

    Rewrited routes to each components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBarComponent from './components/navbar/navbar';
-import MissionPanel from './components/missions/mission-panel';
-import LaunchPanel from './components/launches/launch-panel';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBarComponent from "./components/navbar/navbar";
+import MissionPanel from "./components/missions/mission-panel";
+import LaunchPanel from "./components/launches/launch-panel";
+import Home from "./components/home";
 
 const App: React.FC = () => {
   return (
-    <div className='App'>
-      <NavBarComponent />
-      <Router>
-        <Route path='missions'/>
-        <Route path='launches' />
-      </Router>
-      <MissionPanel></MissionPanel>
-     <LaunchPanel></LaunchPanel>
+    <div className="App">
+      <NavBarComponent></NavBarComponent>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,21 +1,45 @@
-import React, { Component } from 'react';
-import {Navbar, Nav} from 'react-bootstrap';
-const logo = require('../../spaceX_logo.png');
+import React, { Component } from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "../home";
+import MissionPanel from "../missions/mission-panel";
+import LaunchPanel from "../launches/launch-panel";
+const logo = require("../../spaceX_logo.png");
 
 class NavBarComponent extends Component {
-    render() {
-        return (
-          <Navbar bg='dark' variant='dark'>
-            <Navbar.Brand href='/'>
-              <img alt='spaceX' src={logo} width='150' height='45' className='d-inline-block align-top' />
-            </Navbar.Brand>
-            <Nav className='mr-auto'>
-              <Nav.Link href='/missions'>Missions</Nav.Link>
-              <Nav.Link href='/launches'>Launches</Nav.Link>
-            </Nav>
-          </Navbar>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="/">
+            <img
+              alt="spaceX"
+              src={logo}
+              width="150"
+              height="45"
+              className="d-inline-block align-top"
+            />
+          </Navbar.Brand>
+
+          <Nav className="mr-auto">
+            <Nav.Link href="/missions">Missions</Nav.Link>
+            <Nav.Link href="/launches">Launches</Nav.Link>
+          </Nav>
+        </Navbar>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/missions">
+            <MissionPanel />
+          </Route>
+          <Route exact path="/launches">
+            <LaunchPanel />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default NavBarComponent;
