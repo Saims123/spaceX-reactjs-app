07174ef1641a4e43d604a1a98737c07524a3925b
Saims123 <stevesim@hotmail.com>
2020-01-27 16:24:21 +0000

    Temporary workaround to reroute ghp 404 to copy of index.html

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,38 +1,45 @@
 import React, { Component } from "react";
 import { Navbar, Nav } from "react-bootstrap";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "../home";
 import MissionPanel from "../missions/mission-panel";
 import LaunchPanel from "../launches/launch-panel";
-import FullDetailedLaunch from '../launches/full-detail-launch';
-import env from '../../../package.json';
+import FullDetailedLaunch from "../launches/full-detail-launch";
+import env from "../../../package.json";
 const logo = require("../../spaceX_logo.png");
 
 class NavBarComponent extends Component {
   render() {
     return (
-      <Router basename="/">
-        <Navbar bg='dark' variant='dark'>
+      <Router basename={env.homepage}>
+        <Navbar bg="dark" variant="dark">
           <Navbar.Brand href={`${env.homepage}`}>
-            <img alt='spaceX' src={logo} width='150' height='45' className='d-inline-block align-top' />
+            <img
+              alt="spaceX"
+              src={logo}
+              width="150"
+              height="45"
+              className="d-inline-block align-top"
+            />
           </Navbar.Brand>
 
-          <Nav className='mr-auto'>
-            <Nav.Link href={`${env.homepage}/missions`}>Missions</Nav.Link>
-            <Nav.Link href={`${env.homepage}/launches`}>Launches</Nav.Link>
+          <Nav className="mr-auto">
+            <Nav.Item><Link to="/missions">Missions</Link></Nav.Item>
+            <Nav.Item><Link to="/launches">Launches</Link></Nav.Item>
           </Nav>
         </Navbar>
         <Switch>
-          <Route path=''>
-            <Home />
-          </Route>
-          <Route path='missions'>
-            <MissionPanel />
-          </Route>
-          <Route path='launches'>
-            <LaunchPanel />
-          </Route>
-          <Route exact path='launch/:flight_number' component={FullDetailedLaunch}></Route>
+          <Route exact path="/" component={Home}/>
+  
+          <Route path="/missions" component={MissionPanel} />
+
+          <Route path="/launches" component={LaunchPanel} />
+
+          <Route
+            exact
+            path="/launch/:flight_number"
+            component={FullDetailedLaunch}
+          ></Route>
         </Switch>
       </Router>
     );
